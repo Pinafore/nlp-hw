312a98388e359cdaa12afed8ecc902b0bbb9e33f
Neha Srikanth <nehasrik@umd.edu>
2021-12-08 18:15:32 -0500

    reword guess and buzz docstring

--- project/qbmodel.py
+++ project/qbmodel.py
@@ -12,18 +12,20 @@ class QuizBowlModel:
         """
         Load your model(s) and whatever else you need in this function.
 
-        Do NOT load your model or resources in the guess_and_buzz function, as it will
-        increase latency severely. 
+        Do NOT load your model or resources in the guess_and_buzz() function, 
+        as it will increase latency severely. 
         """
         pass
 
     def guess_and_buzz(self, question_text: List[str]) -> List[Tuple[str, bool]]:
         """
-        This function accepts list of question string, and returns a string 
-        representing the guess and a corresponding boolean representing 
+        This function accepts a list of question strings, and returns a list of tuples containing
+        strings representing the guess and corresponding booleans representing 
         whether or not to buzz. 
 
-        If you are using a deep learning model, try to 
-        use batched prediction instead of iterating using a for loop.
+        So, guess_and_buzz(["This is a question"]) should return [("answer", False)]
+
+        If you are using a deep learning model, try to use batched prediction instead of 
+        iterating using a for loop.
         """
         pass

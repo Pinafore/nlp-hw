ae2546b40baf9a7098898ed8f11ba3b932bad5fc
Neha Srikanth <nehasrik@umd.edu>
2021-12-08 18:05:56 -0500

    qbmodel skeleton for final project

---
+++ project/qbmodel.py
@@ -0,0 +1,29 @@
+from typing import List, Tuple
+import nltk
+import sklearn
+import transformers
+import numpy as np
+import pandas as pd
+
+
+class QuizBowlModel:
+
+	def __init__(self):
+		"""
+		Load your model(s) and whatever else you need in this function.
+
+		Do NOT load your model or resources in the guess_and_buzz function, as it will
+		increase latency severely. 
+		"""
+		pass
+
+	def guess_and_buzz(self, question_text: List[str]) -> List[Tuple[str, bool]]:
+		"""
+		This function accepts list of question string, and returns a string 
+		representing the guess and a corresponding boolean representing 
+		whether or not to buzz. 
+
+		If you are using a deep learning model, try to 
+		use batched prediction instead of iterating using a for loop.
+		"""
+		pass

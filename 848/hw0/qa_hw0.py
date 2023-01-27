2325fcd8508a8f9c68a406528c75fc6202d28419
Jordan Boyd-Graber <jbg@boydgraber.org>
2023-01-27 14:58:18 -0500

    wrong class

--- 848/hw0/qa_hw0.py
+++
@@ -1,8 +0,0 @@
-class SimpleQARunner:
-
-    def __init__(self):
-        """Default Constructor for SimpleQARunner."""
-        pass
-
-    def execute_query(self, question_text: str) -> str:
-        """Complete this method to achieve the desired behavior from SimpleQARunner."""
